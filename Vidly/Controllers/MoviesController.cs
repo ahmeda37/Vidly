ef8df14db610299a38ca1f9741531af08d2a91c4
Abdurahman Ahmed <abdahmed@live.ca>
2019-05-03 00:22:01 -0500

    third commit

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,27 +4,22 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies/Random
-        public ActionResult Random()
+        public ActionResult Index()
         {
-            var movie = new Movie() {Name = "Shrek!" };
+            var movie = new Movie() { Name = "Shrek!" };
             return View(movie);
-        }
-        public ActionResult Edit(int id)
-        {
-            return Content("id= " + id);
-        }
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue) { pageIndex = 1;}
-            if (String.IsNullOrWhiteSpace(sortBy)) { sortBy = "Name"; }
-            return Content(String.Format("pageIndex={0}&sortBy{1}", pageIndex, sortBy));
 
+        }
+        [Route("movies/released/{year:regex(\\d{4}):range(1900,2019)}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
         }
     }
 }
